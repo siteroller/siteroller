f7f42439dfee58ed6c3a2f8061506acaa8822bb4
Marcel Haerry <haerry@puzzle.ch>
2009-03-19 11:18:58 +0100

    automagically select all agents to be informed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function toggle_ticket(ticket_id) {
   document.getElementById(ticket_id).toggle();
 }
+
+function selectInformAgents() {
+    $$('select[name="InformUserID"] option').each(function(element){
+        element.selected = true;
+    });
+}
+
+function bodyOnload(){
+    selectInformAgents();
+}
