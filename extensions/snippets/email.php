ce01d017a7a673918ff99c3dbc359852622b8165
SamGoody <siteroller@gmail.com>
2011-09-08 06:17:17 -0500

    Changes to email.php done way back.

--- extensions/snippets/email.php
+++ extensions/snippets/email.php
@@ -4,8 +4,8 @@ function clean($received){
 	foreach (array('name','subject','email','body') as $v) $$v = $received[$v];
 	if (!preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/is', $email)) return 'Please use a standard email address.';
 	foreach (array($email, $name, $subject) as $str) if (preg_match('/(%0A|%0D|\\n+|\\r+)/i', $str)) return 'Multi-line fields are not supported.';
-	$injects = '/multipart\/mixed|(content-transfer-encoding|content-type|mime-version|bcc|cc|to):/is';    
-	foreach (array($email, $name, $subject, $body) as $str) if (!$str || preg_match($injects, $str)) return 'Copies may not be sent via this form.';
+	$injects = '/multipart\/mixed|(content-transfer-encoding|content-type|mime-version|bcc|cc|to):/is';
+	foreach (array($name, $subject, $email, $body) as $str) if (!$str || preg_match($injects, $str)) return 'Copies may not be sent via this form.'.$str.','.preg_match($injects, $str);
 }
 
 function responder($fromAcct, $toAdmin, $received, $response){
